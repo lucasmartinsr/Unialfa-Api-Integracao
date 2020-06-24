428a7bf9d4e32f801b4d6003f64292e8dc316c46
Lucas Martins <lucas_mr92@hotmail.com>
2020-06-23 21:05:41 -0300

    Add files via upload

---
+++ Unialfa.Contato/Unialfa.Contato/Contato.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Unialfa.Contato
+{
+    public class Contato
+    {
+        public string Cpf { get;set;}
+
+        public string Telefone { get;set;}
+    }
+}
---
+++ Unialfa.Contato/Unialfa.Contato/Controllers/ExtracaoContatoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Unialfa.Contato.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExtracaoContatoController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get(int cpf)
+        {
+            return new List<Contato>
+            {
+                new Contato{ Cpf = "01234567800", Telefone = "6299990000" },
+                new Contato{ Cpf = "01234567800", Telefone ="6299990001" },
+                new Contato{ Cpf = "01234567800", Telefone ="6299990002" },
+                 new Contato{ Cpf = "98745612300", Telefone ="6299990003" },
+                 new Contato{ Cpf = "98745612300", Telefone ="6299990004" },
+                 new Contato{ Cpf = "98745612300", Telefone ="6299990005" }
+             }.Where(c => c.Cpf == cpf.ToString()).Select(c => c.Telefone).ToList();
+        }
+    }
+}
